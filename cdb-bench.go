8abf5488ce67dc533d7d8d15b60bfaf7fda78ff8
Alejandro Martínez <zen@itram.es>
2015-06-15 10:32:37 +0200

    Add bbox support

--- cdb-bench.go
+++ cdb-bench.go
@@ -9,8 +9,41 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
+
+type Bbox struct {
+	min_x int
+	min_y int
+	max_x int
+	max_y int
+	zoom  int
+}
+
+func (bbox *Bbox) BboxFromString(bbox_str string, zoom int) {
+	splitted := strings.Split(bbox_str, ",")
+	bbox.zoom = zoom
+	bbox.min_x, _ = strconv.Atoi(splitted[0])
+	bbox.min_y, _ = strconv.Atoi(splitted[1])
+	bbox.max_x, _ = strconv.Atoi(splitted[2])
+	bbox.max_y, _ = strconv.Atoi(splitted[3])
+	if bbox.max_x == -1 {
+		bbox.max_x = int(math.Pow(float64(2), float64(bbox.zoom))) - 1
+	}
+	if bbox.max_y == -1 {
+		bbox.max_y = int(math.Pow(float64(2), float64(bbox.zoom))) - 1
+	}
+	if len(splitted) == 5 {
+		bbox.zoom, _ = strconv.Atoi(splitted[4])
+		// Get multiplication factor to scale bbox to:
+		bbox.max_y = int(math.Ceil(float64(bbox.max_y) * (math.Pow(float64(2), float64(zoom-bbox.zoom)))))
+		bbox.max_x = int(math.Ceil(float64(bbox.max_x) * (math.Pow(float64(2), float64(zoom-bbox.zoom)))))
+		bbox.min_y = int(math.Floor(float64(bbox.min_y) * (math.Pow(float64(2), float64(zoom-bbox.zoom)))))
+		bbox.min_x = int(math.Floor(float64(bbox.min_x) * (math.Pow(float64(2), float64(zoom-bbox.zoom)))))
+	}
+}
 
 func file(name string, create bool) (*os.File, error) {
 	switch name {
@@ -37,31 +70,28 @@ func targetFactory(base string, hostname string) vegeta.Targeter {
 	}
 }
 
-func getTile(boundaries [][]int, tile int) (x int, y int) {
+func (boundaries *Bbox) getTile(tile int) (x int, y int) {
 	// first, trim by the max of them
-	xSize := (boundaries[1][0] - boundaries[0][0])
-	ySize := (boundaries[1][1] - boundaries[0][1])
+	xSize := (boundaries.max_x - boundaries.min_x + 1)
+	ySize := (boundaries.max_y - boundaries.min_y + 1)
 	maxTrim := xSize * ySize
 	tile = tile % maxTrim
-	y = (tile / xSize) + boundaries[0][1]
-	x = (tile % xSize) + boundaries[0][0]
+	y = (tile / xSize) + boundaries.min_y
+	x = (tile % xSize) + boundaries.min_x
 	return
 }
 
-func tileXYZTargetFactory(base string, hostname string, layergroup string, zoom int, seed int) vegeta.Targeter {
-	// The max x/y should be 2^zoom
-	max_coord := int(math.Pow(2, float64(zoom)))
+func tileXYZTargetFactory(base string, hostname string, layergroup string, zoom int, seed int, boundaries Bbox) vegeta.Targeter {
 	// We build the hOST in the header
 	headers := make(http.Header)
 	headers.Add("Host", hostname)
 	r := rand.New(rand.NewSource(int64(seed)))
 	//boundaries := [][]int{{157, 283}, {354, 436}}
 	//boundaries := [][]int{{9, 21}, {17, 27}} //TODO: add a parameter to customize this
-	boundaries := [][]int{{0, 0}, {max_coord, max_coord}}
 	return func() (*vegeta.Target, error) {
 		//r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		// We reuse the random seed to obtain X,Y in tile range
-		x, y := getTile(boundaries, r.Int())
+		x, y := boundaries.getTile(r.Int())
 		// We generate random X/Y from 0 to 2^zoom
 		url := fmt.Sprintf("%s/api/v1/map/%s/%d/%d/%d.png?_cache_bust=%f", base, layergroup, zoom,
 			x, y, r.Float64())
@@ -82,6 +112,8 @@ func main() {
 	var rate int
 	var seed int
 	var zoom int
+	var bbox_string string
+	var bbox Bbox
 
 	var BenchCmd = &cobra.Command{
 		Use:   "cdb-bench",
@@ -97,7 +129,8 @@ func main() {
 		Short: "Request random tiles in a zoom level given a layergroup",
 		Run: func(cmd *cobra.Command, args []string) {
 
-			targeter := tileXYZTargetFactory(base, hostname, layergroup, zoom, seed)
+			bbox.BboxFromString(bbox_string, zoom)
+			targeter := tileXYZTargetFactory(base, hostname, layergroup, zoom, seed, bbox)
 			attacker := vegeta.NewAttacker()
 
 			out, _ := file("stdout", true)
@@ -116,6 +149,7 @@ func main() {
 	tilesCmd.Flags().IntVarP(&zoom, "zoom", "z", 10, "Zoom level")
 	tilesCmd.Flags().IntVarP(&seed, "seed", "s", int(time.Now().UnixNano()), "Random seed (default value is unix time in msecs)")
 	tilesCmd.Flags().StringVarP(&layergroup, "layergroup", "l", "0:0", "Layergroup")
+	tilesCmd.Flags().StringVarP(&bbox_string, "bbox", "x", "0,0,-1,-1", "Bounding box, XYXY (add a fitfth parameter to specify zoom if you want it to be translated to other zoom)")
 
 	BenchCmd.AddCommand(tilesCmd)
 	BenchCmd.Execute()
